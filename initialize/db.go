9080f6597b7884e6bf8ece788ecdc783c9d3bf60
raflinoob132 <limandijaya@gmail.com>
2025-02-16 15:43:44 +0700

    Migrate from MySQL to PostgreSQL

--- initialize/db.go
+++ initialize/db.go
@@ -8,7 +8,8 @@ import (
 	"github.com/raflinoob132/go-notes/initialize/dbmodel"
 	"github.com/raflinoob132/go-notes/models"
 
-	"gorm.io/driver/mysql"
+	// Ganti dari mysql ke postgres
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -19,24 +20,28 @@ var favoriteModel = &models.Favorite{}
 
 func ConnectDB(config *dbmodel.Config) {
 	var err error
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUserName, config.DBUserPassword, config.DBHost, config.DBPort, config.DBName)
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// Format DSN untuk PostgreSQL di Railway
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.DBHost, config.DBPort, config.DBUserName, config.DBUserPassword, config.DBName)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}) // Gunakan driver postgres
 	if err != nil {
-		log.Fatal("Gagal hubungkan ke database" + err.Error())
-		//os.Exit(1)
+		log.Fatal("Gagal hubungkan ke database: " + err.Error())
 	}
+
 	log.Println("Migrasi...")
 	if err := DB.AutoMigrate(noteModel, usersModel, favoriteModel); err != nil {
-		log.Fatal("Gagal migrasi tabel" + err.Error())
+		log.Fatal("Gagal migrasi tabel: " + err.Error())
 	}
 
 	log.Println("Berhasil migrasi")
 }
+
 func LoadConfig() (config dbmodel.Config, err error) {
-	config.DBHost = os.Getenv("MYSQLHOST")
-	config.DBUserName = os.Getenv("MYSQLUSER")
-	config.DBUserPassword = os.Getenv("MYSQL_PASSWORD")
-	config.DBName = os.Getenv("MYSQLNAME")
-	config.DBPort = os.Getenv("MYSQLPORT")
+	config.DBHost = os.Getenv("PGHOST")             // Ganti MYSQLHOST -> PGHOST
+	config.DBUserName = os.Getenv("PGUSER")         // Ganti MYSQLUSER -> PGUSER
+	config.DBUserPassword = os.Getenv("PGPASSWORD") // Ganti MYSQL_PASSWORD -> PGPASSWORD
+	config.DBName = os.Getenv("PGDATABASE")         // Ganti MYSQLNAME -> PGDATABASE
+	config.DBPort = os.Getenv("PGPORT")             // Ganti MYSQLPORT -> PGPORT
 	return
 }
